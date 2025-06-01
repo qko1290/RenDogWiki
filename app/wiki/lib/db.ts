bdf5c474e6e2b46acac8fb420598b709d99c428d
shkim <shkim_1217@naver.com>
2025-06-01 17:11:08 +0900

    DB 세팅 완료

--- app/wiki/lib/db.ts
+++ app/wiki/lib/db.ts
@@ -5,15 +5,7 @@
  * - DB 접속 정보는 환경변수로 관리
  */
 
-import mysql from 'mysql2/promise';
-
 // DB 연결 풀 생성
-export const db = mysql.createPool({
-  host: process.env.DB_HOST!,
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
-  charset: 'utf8mb4',
-  waitForConnections: true,
-  connectionLimit: 10, // 동시 커넥션 제한
-});
+
+import mysql from 'mysql2/promise';
+export const db = mysql.createPool(process.env.DATABASE_URL!);
