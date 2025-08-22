80d312a70490c9e066981d8d3c28c1932aa4c1b7
shkim <shkim_1217@naver.com>
2025-08-22 19:05:56 +0900

    버그수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'd1y7k8qotewoph.cloudfront.net',
+        hostname: 'd1y7k8qotewoph.cloudfront.net', // CloudFront 도메인
       },
+      {
+        protocol: 'https',
+        hostname: 'rendog-wiki-images.s3.amazonaws.com', // 혹시 직접 참조할 때 대비
+      }
     ],
   },
 };
