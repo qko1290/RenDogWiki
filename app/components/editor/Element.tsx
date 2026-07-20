5c51044b1054d2ff08ccec33d98bef37c0804d4e
shkim <shkim_1217@naver.com>
2026-07-20 19:28:05 +0900

    refactor: move table rendering into editor adapter

--- app/components/editor/Element.tsx
+++ app/components/editor/Element.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import React from 'react';
-import {
-  RenderElementProps,
-  useSlate,
-} from 'slate-react';
+import { RenderElementProps, useSlate } from 'slate-react';
 
 import type {
   InlineMarkElement,
@@ -23,37 +20,36 @@ import type {
 } from '@/types/slate';
 
 import type { PriceTableEditState } from './render/types';
-import type { ElementRenderProps, WikiRefKind } from './render/types';
-
-import {
-  TableElementRenderer,
-  TableRowRenderer,
-  TableCellRenderer,
-} from './render/Table';
-
-import PriceTableEditorAdapter from './render/price-table/PriceTableEditorAdapter';
-import WeaponEditorAdapter from './render/weapon/WeaponEditorAdapter';
+import type { ElementRenderProps } from './render/types';
 
 import InlineLinkRenderer from '@/components/wiki-render/link/InlineLinkRenderer';
-import LinkBlockEditorAdapter from './render/link/LinkBlockEditorAdapter';
-import { ImageBlock, VideoBlock } from './render/media/MediaEditorBlocks';
 
 import ParagraphEditorAdapter from './render/blocks/ParagraphEditorAdapter';
 import HeadingEditorAdapter from './render/blocks/HeadingEditorAdapter';
-import LinkBlockRowEditorAdapter from './render/link/LinkBlockRowEditorAdapter';
-
 import {
   DividerEditorAdapter,
   InfoBoxEditorAdapter,
 } from './render/blocks/BasicBlockEditorAdapters';
 
-import FootnoteEditorAdapter from './render/inline/FootnoteEditorAdapter';
+import LinkBlockEditorAdapter from './render/link/LinkBlockEditorAdapter';
+import LinkBlockRowEditorAdapter from './render/link/LinkBlockRowEditorAdapter';
 
+import { ImageBlock, VideoBlock } from './render/media/MediaEditorBlocks';
+
+import FootnoteEditorAdapter from './render/inline/FootnoteEditorAdapter';
 import {
   InlineImageEditorAdapter,
   InlineMarkEditorAdapter,
   WikiRefEditorAdapter,
 } from './render/inline/InlineEditorAdapters';
+
+import PriceTableEditorAdapter from './render/price-table/PriceTableEditorAdapter';
+import {
+  TableEditorAdapter,
+  TableRowEditorAdapter,
+  TableCellEditorAdapter,
+} from './render/table/TableEditorAdapter';
+import WeaponEditorAdapter from './render/weapon/WeaponEditorAdapter';
 
 import EmbedPlaceholderEditorAdapter from './render/embed/EmbedPlaceholderEditorAdapter';
 
@@ -84,7 +80,9 @@ const Element: React.FC<ElementRenderProps> = ({
         <InlineLinkRenderer
           mode="edit"
           href={(element as any).url}
-          attributes={attributes as React.AnchorHTMLAttributes<HTMLAnchorElement>}
+          attributes={
+            attributes as React.AnchorHTMLAttributes<HTMLAnchorElement>
+          }
         >
           {children}
         </InlineLinkRenderer>
@@ -136,10 +134,7 @@ const Element: React.FC<ElementRenderProps> = ({
 
     case 'divider': {
       return (
-        <DividerEditorAdapter
-          attributes={attributes}
-          element={element}
-        >
+        <DividerEditorAdapter attributes={attributes} element={element}>
           {children}
         </DividerEditorAdapter>
       );
@@ -147,10 +142,7 @@ const Element: React.FC<ElementRenderProps> = ({
 
     case 'info-box': {
       return (
-        <InfoBoxEditorAdapter
-          attributes={attributes}
-          element={element}
-        >
+        <InfoBoxEditorAdapter attributes={attributes} element={element}>
           {children}
         </InfoBoxEditorAdapter>
       );
@@ -225,33 +217,36 @@ const Element: React.FC<ElementRenderProps> = ({
 
     case 'table': {
       return (
-        <TableElementRenderer
+        <TableEditorAdapter
           attributes={attributes}
           element={element as TableElement}
           editor={editor}
         >
           {children}
-        </TableElementRenderer>
+        </TableEditorAdapter>
       );
     }
 
     case 'table-row': {
       return (
-        <TableRowRenderer attributes={attributes} element={element as any}>
+        <TableRowEditorAdapter
+          attributes={attributes}
+          element={element as any}
+        >
           {children}
-        </TableRowRenderer>
+        </TableRowEditorAdapter>
       );
     }
 
     case 'table-cell': {
       return (
-        <TableCellRenderer
+        <TableCellEditorAdapter
           attributes={attributes}
           element={element as any}
           editor={editor}
         >
           {children}
-        </TableCellRenderer>
+        </TableCellEditorAdapter>
       );
     }
 
